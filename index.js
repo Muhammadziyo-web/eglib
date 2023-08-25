cbf42c4169e7805006714e61375e5bc2a67b93f1
Muhammadziyo-web <mrvaliyev2006@gmail.com>
2023-08-25 10:48:07 +0500

    Src and Server.js creator

--- index.js
+++ index.js
@@ -2,22 +2,17 @@
 
 const fs = require("fs");
 const path = require("path");
-
 const { program } = require("commander");
-
-
-
 
 program
   .command("create")
   .description("Generate custom files and folders")
   .action(() => {
-    fs.mkdirSync('src');
-    const filePath = path.join(folderName, "server.js");
-    fs.writeFileSync(filePath, "console.log(Hello world!");
-
+    let mainFolder = "src";
+    fs.mkdirSync(mainFolder);
+    const filePath = path.join(mainFolder, "server.js");
+    fs.writeFileSync(filePath, "console.log('Hello world!')");
     console.log("Generated custom files and folders.");
   });
-
 
 program.parse(process.argv);
