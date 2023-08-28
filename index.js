07004ce9ef5e10fa702895918c8ed2e31938b404
Muhammadziyo-web <mrvaliyev2006@gmail.com>
2023-08-28 21:27:18 +0500

    Cloning from template

--- index.js
+++ index.js
@@ -3,16 +3,39 @@
 const fs = require("fs");
 const path = require("path");
 const { program } = require("commander");
-const simpleGit = require("simple-git"); 
+const simpleGit = require("simple-git");
+const { execSync } = require("child_process")
+
 program
-  .command("create")
+  .command("create <project-name")
   .description("Generate custom files and folders")
-  .action(() => {
-    let mainFolder = "src";
-    fs.mkdirSync(mainFolder);
-    const filePath = path.join(mainFolder, "server.js");
-    fs.writeFileSync(filePath, "console.log('Hello world!')");
-    console.log("Generated custom files and folders.");
+  .action(projectName) => {
+    console.log("\x1b[33m Welcome to the eglib! \x1b[0m");
+    const templateRepoUrl =
+      "https://github.com/Muhammadziyo-web/eglib-template.git";
+  const projectName = "projectName"; // You can customize this
+
+  const targetDir = path.join(process.cwd(), projectName);
+
+  console.log(`\x1b[33m Cloning template from ${templateRepoUrl}... \x1b[0m`);
+
+  simpleGit().clone(templateRepoUrl, targetDir, (err) => {
+    if (err) {
+      console.error("Error cloning template:", err);
+    } else {
+      console.log("Template cloned successfully.");
+
+      process.chdir(targetDir);
+      console.log("\x1b[33m Installing dependencies... \x1b[0m");
+      try {
+        execSync("npm install", { stdio: "inherit" });
+        console.log("Dependencies installed successfully.");
+        execSync("npm run start", { stdio: "inherit" });
+      } catch (error) {
+        console.error("Error installing dependencies:", error);
+      }
+    }
+  });
   });
 
 program.parse(process.argv);
